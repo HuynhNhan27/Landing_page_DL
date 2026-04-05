ae07497e0924bb4d3c461599be6df0fe5cc1de0b
HuynhNhan27 <nhan.huynhgl272@hcmut.edu.vn>
2026-04-05 22:37:25 +0700

    change link

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/DeepLearning/",
+  base: "/Landing_page_DL/",
   server: {
     host: "::",
     port: 8080,
